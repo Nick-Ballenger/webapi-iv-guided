bebcca0a6bfa94ef1af4e276cd19b180309f2a00
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-04-25 12:17:15 -0500

    trying again

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const envReader = require('dotenv');
+envReader.config();
 const server = require('./api/server.js');
-const port = process.env.PORT||4000;
+const port = process.env.PORT||5000;
 
 server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
